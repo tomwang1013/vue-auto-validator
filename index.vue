250202ee5a2137d31c85d3209396ecac1a30b7fc
tomwang <174604588@qq.com>
2017-03-23 15:56:31 +0800

    more init code

--- index.vue
+++ index.vue
@@ -1,28 +1,21 @@
 <template>
+  <form @submit='onsubmit' @focusout='onfocusout' @change='onchange' @input='oninput' ref='fm'>
+    <slot>all fields here</slot>
+  </form>
 </template>
 
 <script>
-  var ErrMsg = {
-    template: "<span v-show='isShow' :class='errorClass'>{{message}}</span>",
-    data: function() {
-      return {
-        isShow: false,
-        message: ''
-      }
-    },
-    props: {
-      errorClass: {
-        type: String,
-        required: true
-      }
-    }
-  };
+  var _ = require('lodash');
+  var Vue = require('vue');
+  var methods = require('./lib/validate_methods.js');
+  var ErrMsg = require('./lib/validate_methods.js');
 
   module.exports = {
     name: 'form-validator',
 
     data: function() {
       return {
+        errors: {}
       };
     },
 
@@ -69,13 +62,69 @@
       }
     },
 
-    computed: {
-    },
-
     methods: {
+      onsubmit: function(evt) {
+        var fm = this.$refs.fm;
+        var me = this;
+        var lastInvalidName = '';
+
+        _.forOwn(this.rules, function(rule, name) {
+          _forOwn(rule, function(methodName, args) {
+            if (methods[methodName].apply(fm[name], args)) {
+              me.errors[name].hide();
+            } else {
+              me.errors[name].show(me.messages[name][methodName]);
+              lastInvalidName = name;
+            }
+          });
+        })
+
+        if (lastInvalidName) {
+          fm[lastInvalidName].focus();
+          e.preventDefault();
+          return;
+        }
+
+        if (this.submitHandler) {
+          this.submitHandler(fm);
+          e.preventDefault();
+          return;
+        }
+
+        fm.submit();
+      },
+
+      onfocusout: function(evt) {
+      },
+
+      onchange: function(evt) {
+      },
+
+      oninput: function(evt) {
+      }
     },
 
     mounted: function() {
+      var fm = this.$refs.fm;
+      var me = this;
+
+      _.forOwn(this.rules, function(rule, name) {
+        var field = fm[name];
+        var mp = window.document.createElement('span');
+
+        if (me.errorPlacement == 'after') {
+          field.parentNode.appendChild(mp);
+        } else {
+          field.parentNode.insertBefore(mp, field);
+        }
+
+        me.errors[name] = new Vue({
+          el: mp,
+          render: function(h) {
+            return h(ErrMsg, { props: { errorClass: me.errorClass } });
+          }
+        });
+      });
     }
   };
 </script>
---
+++ lib/error_message.vue
@@ -0,0 +1,32 @@
+<template>
+  <span v-show='isShow' :class='errorClass'>{{message}}</span>
+</template>
+
+<script>
+  module.exports = {
+    name: 'fm-validation-error-msg',
+    data: function() {
+      return {
+        isShow: false,
+        message: ''
+      }
+    },
+    props: {
+      errorClass: {
+        type: String,
+        required: true
+      }
+    },
+
+    methods: {
+      hide: function() {
+        this.isShow = false;
+      },
+
+      show: function(message) {
+        this.message = message;
+        this.isShow = true;
+      }
+    }
+  };
+</script>
