799f2bd1f4cf6dc43314bf4c8b1d19a4504efe09
wangxiantong <wangxiantong@cd.tuan800.com>
2017-04-05 17:47:04 +0800

    init

--- index.vue
+++ index.vue
@@ -2,6 +2,22 @@
 </template>
 
 <script>
+  var ErrMsg = {
+    template: "<span v-show='isShow' :class='errorClass'>{{message}}</span>",
+    data: function() {
+      return {
+        isShow: false,
+        message: ''
+      }
+    },
+    props: {
+      errorClass: {
+        type: String,
+        required: true
+      }
+    }
+  };
+
   module.exports = {
     name: 'form-validator',
 
@@ -11,12 +27,55 @@
     },
 
     props: {
+      // form submitter to replace the default submit
+      submitHandler: {
+        type: Function
+      },
+
+      // callback when the form validation failed
+      invalidHandler: {
+        type: Function
+      },
+
+      // validation rules
+      rules: {
+        type: Object,
+      },
+
+      // messages when validation failed
+      messages: {
+        type: Object,
+      },
+
+      errorClass: {
+        type: String,
+        default: 'o-error-field'
+      },
+
+      validClass: {
+        type: String,
+        default: 'o-valid-field'
+      },
+
+      // where to place the error message: before, after
+      errorPlacement: {
+        type: String,
+        default: 'after'
+      },
+
+      // let user control the error display totally
+      showErrors: {
+        type: Function
+      }
     },
 
     computed: {
     },
 
     methods: {
+    },
+
+    mounted: function() {
     }
   };
 </script>
