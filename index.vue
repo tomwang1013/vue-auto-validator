43458803d02048d62c4aa84850d1e67a152fd8ae
wangxiantong <wangxiantong@cd.tuan800.com>
2017-04-05 10:27:19 +0800

    empty file

---
+++ index.vue
@@ -0,0 +1,25 @@
+<template>
+</template>
+
+<script>
+  module.exports = {
+    name: 'form-validator',
+
+    data: function() {
+      return {
+      };
+    },
+
+    props: {
+    },
+
+    computed: {
+    },
+
+    methods: {
+    }
+  };
+</script>
+
+<style lang='sass'>
+</style>
